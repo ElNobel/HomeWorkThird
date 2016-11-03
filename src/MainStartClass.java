15ce7ba97f0b6adeb14b1d0b23abe47f13023ae5
Nikita Titov <nikitaunmortal@gmail.com>
2016-11-03 13:07:40 +0300

    Исправление недочётов

--- src/MainStartClass.java
+++ src/MainStartClass.java
@@ -5,19 +5,19 @@ public class MainStartClass {
         int[][] secondMatrix = new int[5][5];
         MultiMatrix.doRandomMatrix(firstMatrix);
         MultiMatrix.doRandomMatrix(secondMatrix);
-        int[][] result =MultiMatrix.multiplyMatrix(firstMatrix,secondMatrix);
+        int[][] result = MultiMatrix.multiplyMatrix(firstMatrix, secondMatrix);
         MultiMatrix.printResult(result);
         System.out.println();
 
         int[][] firstTestMatrix = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
         };
         int[][] secondTestMatrix = {
-                {9,8,7},
-                {6,5,4},
-                {3,2,1}
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}
         };
 
         System.out.println(MultiMatrix.testMultyMatrix(firstTestMatrix, secondTestMatrix));
